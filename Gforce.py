f89f8a15f76d3519a9a62a61deba5710adc8f5fa
ArjunSridharkumar <98350094+ArjunSridharkumar@users.noreply.github.com>
2023-08-11 03:02:18 -0400

    Update Gforce.py

--- Gforce.py
+++ Gforce.py
@@ -32,6 +32,7 @@ from langchain.text_splitter import CharacterTextSplitter
 
 
 openai_api_key = st.secrets["OPENAI_API_KEY"]
+deeplake_key = st.secrets["ACTIVELOOP_TOKEN"]
 # QDRANT_COLLECTION ="resume"
 
 
@@ -87,7 +88,7 @@ def generate_response(doc_texts, openai_api_key, query_text):
     # db = Chroma.from_documents(texts, embeddings)
     # Create retriever interface
     # retriever = db.as_retriever(search_type="similarity")
-    db = DeepLake(dataset_path="hub://arjunsridhar9720/twitter-algorithm_45", read_only=True, embedding_function=embeddings)
+    db = DeepLake(dataset_path="hub://arjunsridhar9720/twitter-algorithm_45", token = deeplake_key,read_only=True, embedding_function=embeddings)
     retriever = db.as_retriever()
     retriever.search_kwargs['distance_metric'] = 'cos'
     retriever.search_kwargs['fetch_k'] = 100
